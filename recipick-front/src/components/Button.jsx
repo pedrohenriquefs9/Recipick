e0d71d5ff7e305edb657aaea6ccde40346f67e2b
Welton Felix <wplf@cin.ufpe.br>
2025-06-30 22:19:04 -0300

    front(chore): standardize string quotes in Button component

--- recipick-front/src/components/Button.jsx
+++ recipick-front/src/components/Button.jsx
@@ -1,8 +1,8 @@
-import clsx from "clsx";
+import clsx from 'clsx';
 
-export function Button({ children, onClick, variant = "primary", ...props }) {
+export function Button({ children, onClick, variant = 'primary', ...props }) {
   const variants = {
-    primary: "bg-primary text-light",
+    primary: 'bg-primary text-light',
   };
 
   return (
@@ -10,11 +10,11 @@ export function Button({ children, onClick, variant = "primary", ...props }) {
       type="button"
       onClick={onClick}
       className={clsx(
-        `cursor-pointer p-2 rounded-full inline-flex justify-center items-center text-center`,
+        'cursor-pointer p-2 rounded-full inline-flex justify-center items-center text-center',
         variants[variant],
-        "hover:opacity-90 transition-opacity duration-200",
-        "focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary",
-        "disabled:opacity-50 disabled:cursor-not-allowed",
+        'hover:opacity-90 transition-opacity duration-200',
+        'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary',
+        'disabled:opacity-50 disabled:cursor-not-allowed',
       )}
       {...props}
     >
